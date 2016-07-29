fa4c735bc97ea5273a487bd964b3a740ef5365d2
miguelrc <miguelrc@kth.se>
2016-07-29 18:07:05 +0200

    Revert again to "Fix in TrafficFileToPWS"
    This reverts commit ae7427dfa1e852e3c728806feda1b95a57d87866.

---
+++ ScriptsAndTools/TrafficFileToPWS/TrafficFileToPWS/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace TrafficFileToPWS
+{
+    partial class TrafficFileToPWS_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chooseFile_textBox = new System.Windows.Forms.TextBox();
+            this.destinationFolder_textBox = new System.Windows.Forms.TextBox();
+            this.chooseFile_searchButton = new System.Windows.Forms.Button();
+            this.destinationFolder_searchButton = new System.Windows.Forms.Button();
+            this.convertToPWS_button = new System.Windows.Forms.Button();
+            this.chooseFile_label = new System.Windows.Forms.Label();
+            this.destinationFolder_label = new System.Windows.Forms.Label();
+            this.message_label = new System.Windows.Forms.Label();
+            this.mbPerFile_numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.mbPerFile_label = new System.Windows.Forms.Label();
+            this.outputPrefix_label = new System.Windows.Forms.Label();
+            this.outputPrefix_textBox = new System.Windows.Forms.TextBox();
+            this.trace_textBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mbPerFile_numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chooseFile_textBox
+            // 
+            this.chooseFile_textBox.Location = new System.Drawing.Point(23, 71);
+            this.chooseFile_textBox.Name = "chooseFile_textBox";
+            this.chooseFile_textBox.Size = new System.Drawing.Size(316, 22);
+            this.chooseFile_textBox.TabIndex = 0;
+            // 
+            // destinationFolder_textBox
+            // 
+            this.destinationFolder_textBox.Location = new System.Drawing.Point(23, 142);
+            this.destinationFolder_textBox.Name = "destinationFolder_textBox";
+            this.destinationFolder_textBox.Size = new System.Drawing.Size(316, 22);
+            this.destinationFolder_textBox.TabIndex = 1;
+            // 
+            // chooseFile_searchButton
+            // 
+            this.chooseFile_searchButton.Location = new System.Drawing.Point(353, 66);
+            this.chooseFile_searchButton.Name = "chooseFile_searchButton";
+            this.chooseFile_searchButton.Size = new System.Drawing.Size(102, 32);
+            this.chooseFile_searchButton.TabIndex = 2;
+            this.chooseFile_searchButton.Text = "search...";
+            this.chooseFile_searchButton.UseVisualStyleBackColor = true;
+            this.chooseFile_searchButton.Click += new System.EventHandler(this.chooseFile_searchButton_Click);
+            // 
+            // destinationFolder_searchButton
+            // 
+            this.destinationFolder_searchButton.Location = new System.Drawing.Point(353, 137);
+            this.destinationFolder_searchButton.Name = "destinationFolder_searchButton";
+            this.destinationFolder_searchButton.Size = new System.Drawing.Size(102, 32);
+            this.destinationFolder_searchButton.TabIndex = 3;
+            this.destinationFolder_searchButton.Text = "search...";
+            this.destinationFolder_searchButton.UseVisualStyleBackColor = true;
+            this.destinationFolder_searchButton.Click += new System.EventHandler(this.destinationFolder_searchButton_Click);
+            // 
+            // convertToPWS_button
+            // 
+            this.convertToPWS_button.Location = new System.Drawing.Point(158, 265);
+            this.convertToPWS_button.Name = "convertToPWS_button";
+            this.convertToPWS_button.Size = new System.Drawing.Size(173, 42);
+            this.convertToPWS_button.TabIndex = 4;
+            this.convertToPWS_button.Text = "Convert to PWS";
+            this.convertToPWS_button.UseVisualStyleBackColor = true;
+            this.convertToPWS_button.Click += new System.EventHandler(this.convertToPWS_button_Click);
+            // 
+            // chooseFile_label
+            // 
+            this.chooseFile_label.AutoSize = true;
+            this.chooseFile_label.Location = new System.Drawing.Point(23, 51);
+            this.chooseFile_label.Name = "chooseFile_label";
+            this.chooseFile_label.Size = new System.Drawing.Size(239, 17);
+            this.chooseFile_label.TabIndex = 5;
+            this.chooseFile_label.Text = "Choose a FCD or FZP file to convert:";
+            // 
+            // destinationFolder_label
+            // 
+            this.destinationFolder_label.AutoSize = true;
+            this.destinationFolder_label.Location = new System.Drawing.Point(23, 122);
+            this.destinationFolder_label.Name = "destinationFolder_label";
+            this.destinationFolder_label.Size = new System.Drawing.Size(254, 17);
+            this.destinationFolder_label.TabIndex = 6;
+            this.destinationFolder_label.Text = "Choose a destination for the PWS files:";
+            // 
+            // message_label
+            // 
+            this.message_label.AutoSize = true;
+            this.message_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.message_label.Location = new System.Drawing.Point(14, 321);
+            this.message_label.MinimumSize = new System.Drawing.Size(450, 0);
+            this.message_label.Name = "message_label";
+            this.message_label.Size = new System.Drawing.Size(450, 18);
+            this.message_label.TabIndex = 7;
+            this.message_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mbPerFile_numericUpDown
+            // 
+            this.mbPerFile_numericUpDown.Location = new System.Drawing.Point(26, 214);
+            this.mbPerFile_numericUpDown.Maximum = new decimal(new int[] {
+            4096,
+            0,
+            0,
+            0});
+            this.mbPerFile_numericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.mbPerFile_numericUpDown.Name = "mbPerFile_numericUpDown";
+            this.mbPerFile_numericUpDown.Size = new System.Drawing.Size(111, 22);
+            this.mbPerFile_numericUpDown.TabIndex = 8;
+            this.mbPerFile_numericUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.mbPerFile_numericUpDown.Value = new decimal(new int[] {
+            128,
+            0,
+            0,
+            0});
+            // 
+            // mbPerFile_label
+            // 
+            this.mbPerFile_label.AutoSize = true;
+            this.mbPerFile_label.Location = new System.Drawing.Point(23, 189);
+            this.mbPerFile_label.Name = "mbPerFile_label";
+            this.mbPerFile_label.Size = new System.Drawing.Size(114, 17);
+            this.mbPerFile_label.TabIndex = 9;
+            this.mbPerFile_label.Text = "MB per PWS file:";
+            // 
+            // outputPrefix_label
+            // 
+            this.outputPrefix_label.AutoSize = true;
+            this.outputPrefix_label.Location = new System.Drawing.Point(155, 189);
+            this.outputPrefix_label.Name = "outputPrefix_label";
+            this.outputPrefix_label.Size = new System.Drawing.Size(93, 17);
+            this.outputPrefix_label.TabIndex = 10;
+            this.outputPrefix_label.Text = "Output prefix:";
+            // 
+            // outputPrefix_textBox
+            // 
+            this.outputPrefix_textBox.Location = new System.Drawing.Point(158, 213);
+            this.outputPrefix_textBox.Name = "outputPrefix_textBox";
+            this.outputPrefix_textBox.Size = new System.Drawing.Size(297, 22);
+            this.outputPrefix_textBox.TabIndex = 11;
+            // 
+            // trace_textBox
+            // 
+            this.trace_textBox.Location = new System.Drawing.Point(17, 352);
+            this.trace_textBox.Multiline = true;
+            this.trace_textBox.Name = "trace_textBox";
+            this.trace_textBox.Size = new System.Drawing.Size(447, 141);
+            this.trace_textBox.TabIndex = 12;
+            // 
+            // TrafficFileToPWS_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 507);
+            this.Controls.Add(this.trace_textBox);
+            this.Controls.Add(this.outputPrefix_textBox);
+            this.Controls.Add(this.outputPrefix_label);
+            this.Controls.Add(this.mbPerFile_label);
+            this.Controls.Add(this.mbPerFile_numericUpDown);
+            this.Controls.Add(this.message_label);
+            this.Controls.Add(this.destinationFolder_label);
+            this.Controls.Add(this.chooseFile_label);
+            this.Controls.Add(this.convertToPWS_button);
+            this.Controls.Add(this.destinationFolder_searchButton);
+            this.Controls.Add(this.chooseFile_searchButton);
+            this.Controls.Add(this.destinationFolder_textBox);
+            this.Controls.Add(this.chooseFile_textBox);
+            this.Name = "TrafficFileToPWS_Form";
+            this.Text = "Traffic File To PWS ";
+            ((System.ComponentModel.ISupportInitialize)(this.mbPerFile_numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox chooseFile_textBox;
+        private System.Windows.Forms.TextBox destinationFolder_textBox;
+        private System.Windows.Forms.Button chooseFile_searchButton;
+        private System.Windows.Forms.Button destinationFolder_searchButton;
+        private System.Windows.Forms.Button convertToPWS_button;
+        private System.Windows.Forms.Label chooseFile_label;
+        private System.Windows.Forms.Label destinationFolder_label;
+        private System.Windows.Forms.Label message_label;
+        private System.Windows.Forms.NumericUpDown mbPerFile_numericUpDown;
+        private System.Windows.Forms.Label mbPerFile_label;
+        private System.Windows.Forms.Label outputPrefix_label;
+        private System.Windows.Forms.TextBox outputPrefix_textBox;
+        private System.Windows.Forms.TextBox trace_textBox;
+
+    }
+}
+
---
+++ ScriptsAndTools/TrafficFileToPWS/TrafficFileToPWS/TrafficFileConverter.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Globalization;
+
+namespace TrafficFileToPWS
+{    
+    /// <summary>
+    /// Controller class for TrafficFileToPWS.
+    /// </summary>
+    class TrafficFileConverter
+    {
+        /// <summary>
+        /// Converts the given FCD input file into one or several PWS output files.
+        /// </summary>
+        /// <param name="pathChoosenFile">Path of the FCD input file.</param>
+        /// <param name="pathDestinationFolder">Destination folder for the PWS output files.</param>
+        /// <param name="outputFilePrefix">Prefix for the output files.</param>
+        /// <param name="bytesPerFile">Max. number of bytes per output file.</param>
+        /// <param name="sender">Object that represents the background worker calling this method.</param>
+        internal void ConvertFromFCD(string pathChoosenFile, string pathDestinationFolder, string outputFilePrefix, long bytesPerFile, object sender)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            List<float> times = new List<float>();
+            List<string> outputFilenames = new List<string>();
+            List<string> vehiclesOfCurrentTimeStep = new List<string>();
+            int timeStepCounter = 0;
+            int firstTimeStepOutputFile = 0;
+
+            FileStream inputFile;
+
+            string pathOutputFile;
+            FileStream descriptorOutputFile;
+            StreamWriter writer;
+
+            XmlReader reader;
+            StreamReader streamReader;
+            bool endReached = false;
+
+            // Open the FCD file and create the XML reader
+            inputFile = File.OpenRead(pathChoosenFile);
+            streamReader = new StreamReader(inputFile);
+            reader = XmlReader.Create(streamReader);
+
+            // Create the first output file
+            pathOutputFile = pathDestinationFolder + "\\" + outputFilePrefix + "_" + firstTimeStepOutputFile + ".pws";
+            Console.WriteLine("Creating new output file");
+            descriptorOutputFile = File.Create(pathOutputFile);
+            writer = new StreamWriter(descriptorOutputFile);
+
+            //If readable XML chunk, process data
+            try
+            {
+                while (!endReached)
+                {
+                    if (reader.CanReadValueChunk)
+                    {
+                        reader.Read();
+
+                        switch (reader.NodeType)
+                        {
+                            //Reading element    
+                            case XmlNodeType.Element:
+
+                                //Reading timeStep
+                                if (reader.Name.Equals("timestep"))
+                                {
+                                    if (times.Count > 0)
+                                    {
+                                        WriteNewTimeStep(timeStepCounter, vehiclesOfCurrentTimeStep, writer);
+                                        vehiclesOfCurrentTimeStep.Clear();
+                                        timeStepCounter++;
+                                    }
+
+                                    // Check the size of the current output file and create a new one if needed
+                                    FileInfo fInfo = new FileInfo(pathOutputFile);
+                                    if (fInfo.Length > bytesPerFile)
+                                    {
+                                        //Give a final name to the file
+                                        outputFilenames.Add(fInfo.Name + " " + firstTimeStepOutputFile + " " + (timeStepCounter - 1));
+
+                                        Console.WriteLine("Creating new output file");
+
+                                        writer.Close();
+                                        descriptorOutputFile.Close();
+                                        firstTimeStepOutputFile = timeStepCounter;
+                                        pathOutputFile = pathDestinationFolder + "\\" + outputFilePrefix + "_" + firstTimeStepOutputFile + ".pws";
+                                        descriptorOutputFile = File.Create(pathOutputFile);
+                                        writer = new StreamWriter(descriptorOutputFile);
+                                    }
+
+
+                                    worker.ReportProgress(timeStepCounter);
+                                    times.Add(float.Parse(reader.GetAttribute("time"), NumberStyles.Any, CultureInfo.InvariantCulture));
+                                }
+
+                                //Reading vehicle
+                                if (reader.Name.Equals("vehicle"))
+                                {
+                                    vehiclesOfCurrentTimeStep.Add(reader.GetAttribute("id") + " "
+                                        + reader.GetAttribute("x") + " "
+                                        + reader.GetAttribute("y") + " "
+                                        + reader.GetAttribute("type") + " "
+                                        + reader.GetAttribute("angle"));
+                                }
+                                break;
+
+                            //Reading end element
+                            case XmlNodeType.EndElement:
+
+                                if (reader.Name.Equals("fcd-export"))
+                                {
+                                    // Write the last timestep
+                                    WriteNewTimeStep(timeStepCounter, vehiclesOfCurrentTimeStep, writer);
+
+                                    FileInfo fInfo = new FileInfo(pathOutputFile);
+                                    outputFilenames.Add(fInfo.Name + " " + firstTimeStepOutputFile + " " + timeStepCounter);
+
+                                    writer.Close();
+                                    descriptorOutputFile.Close();
+                                    pathOutputFile = pathDestinationFolder + "\\" + outputFilePrefix + "_" + ".pws.meta";
+                                    descriptorOutputFile = File.Create(pathOutputFile);
+                                    writer = new StreamWriter(descriptorOutputFile);
+                                    WriteMetaData(times, outputFilenames, writer);
+                                    writer.Close();
+                                    descriptorOutputFile.Close();
+
+                                    endReached = true;
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + " " + ex.StackTrace);
+            }
+            finally
+            {
+                if (writer != null)
+                {
+                    writer.Close();
+                    writer.Dispose();
+                }
+                if (descriptorOutputFile != null)
+                {
+                    descriptorOutputFile.Close();
+                    descriptorOutputFile.Dispose();
+                }
+                if (reader != null)
+                {
+                    reader.Close();
+                    reader.Dispose();
+                }
+                if (streamReader != null)
+                {
+                    streamReader.Close();
+                    streamReader.Dispose();
+                }
+                if (inputFile != null)
+                {
+                    inputFile.Close();
+                    inputFile.Dispose();
+                }
+            }
+
+            worker.CancelAsync();
+        }
+
+        /// <summary>
+        /// Converts the given FZP input file into one or several PWS output files.
+        /// </summary>
+        /// <param name="pathChoosenFile">Path of the FZP input file.</param>
+        /// <param name="pathDestinationFolder">Destination folder for the PWS output files.</param>
+        /// <param name="outputFilePrefix">Prefix for the output files.</param>
+        /// <param name="bytesPerFile">Max. number of bytes per output file.</param>
+        /// <param name="sender">Object that represents the background worker calling this method.</param>
+        internal void ConvertFromFZP(string pathChoosenFile, string pathDestinationFolder, string outputFilePrefix, long bytesPerFile, object sender)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            List<float> times = new List<float>();
+            List<string> outputFilenames = new List<string>();
+            List<string> vehiclesOfCurrentTimeStep = new List<string>();
+            int timeStepCounter = 0;
+            int firstTimeStepOutputFile = 0;
+            float currentTime = 0.0f;
+
+            string pathOutputFile;
+            FileStream descriptorOutputFile;
+            StreamWriter writer;
+            FileInfo fInfo;
+
+            // Create a reader for the input file
+            FileStream fileStream = File.OpenRead(pathChoosenFile);
+            StreamReader reader = new StreamReader(fileStream);
+
+            // Create the first output file
+            pathOutputFile = pathDestinationFolder + "\\" + outputFilePrefix + "_" + firstTimeStepOutputFile + ".pws";
+            Console.WriteLine("Creating new output file");
+            descriptorOutputFile = File.Create(pathOutputFile);
+            writer = new StreamWriter(descriptorOutputFile);
+
+            try
+            {
+                bool startVehicleRead = false;
+                var delims = new char[] { ';', ' ' };
+
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    if (line.StartsWith("*"))
+                    {
+                        continue;
+                    }
+                    if (line.Contains("$VEHICLE"))
+                    {
+                        startVehicleRead = true;
+                        line = reader.ReadLine();
+                        string[] lineWords = line.Split(delims);
+                        currentTime = float.Parse(lineWords[0], NumberStyles.Any, CultureInfo.InvariantCulture);
+                        times.Add(currentTime);
+                    }
+                    if (startVehicleRead)
+                    {
+                        string[] lineWords = line.Split(delims);
+                        float time = float.Parse(lineWords[0], NumberStyles.Any, CultureInfo.InvariantCulture);
+
+                        if (currentTime != time)
+                        {
+                            // Write timestep on file
+                            WriteNewTimeStep(timeStepCounter, vehiclesOfCurrentTimeStep, writer);
+                            vehiclesOfCurrentTimeStep.Clear();
+                            currentTime = time;
+                            times.Add(currentTime);
+                            timeStepCounter++;
+                        }
+
+                        // Check the size of the current output file and create a new one if needed
+                        fInfo = new FileInfo(pathOutputFile);
+                        if (fInfo.Length > bytesPerFile)
+                        {
+                            //Give a final name to the file
+                            outputFilenames.Add(fInfo.Name + " " + firstTimeStepOutputFile + " " + (timeStepCounter - 1));
+
+                            Console.WriteLine("Creating new output file");
+
+                            writer.Close();
+                            descriptorOutputFile.Close();
+                            firstTimeStepOutputFile = timeStepCounter;
+                            pathOutputFile = pathDestinationFolder + "\\" + outputFilePrefix + "_" + firstTimeStepOutputFile + ".pws";
+                            descriptorOutputFile = File.Create(pathOutputFile);
+                            writer = new StreamWriter(descriptorOutputFile);
+                        }
+
+                        worker.ReportProgress(timeStepCounter);
+
+                        vehiclesOfCurrentTimeStep.Add(lineWords[1] + " "
+                            + lineWords[2] + " "
+                            + lineWords[3] + " "
+                            + "VEH_FZP" + " "
+                            + ConvertFZPAngle(float.Parse(lineWords[8], NumberStyles.Any, CultureInfo.InvariantCulture)));
+                    }
+                }
+
+                // Write the last timestep
+                WriteNewTimeStep(timeStepCounter, vehiclesOfCurrentTimeStep, writer);
+
+                fInfo = new FileInfo(pathOutputFile);
+                outputFilenames.Add(fInfo.Name + " " + firstTimeStepOutputFile + " " + timeStepCounter);
+
+                writer.Close();
+                descriptorOutputFile.Close();
+                pathOutputFile = pathDestinationFolder + "\\" + outputFilePrefix + "_" + ".pws.meta";
+                descriptorOutputFile = File.Create(pathOutputFile);
+                writer = new StreamWriter(descriptorOutputFile);
+                WriteMetaData(times, outputFilenames, writer);
+                writer.Close();
+                descriptorOutputFile.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + " " + ex.StackTrace);
+            }
+            finally
+            {
+                if (writer != null)
+                {
+                    writer.Close();
+                    writer.Dispose();
+                }
+                if (descriptorOutputFile != null)
+                {
+                    descriptorOutputFile.Close();
+                    descriptorOutputFile.Dispose();
+                }
+                if (reader != null)
+                {
+                    reader.Close();
+                    reader.Dispose();
+                }
+                if (fileStream != null)
+                {
+                    fileStream.Close();
+                    fileStream.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Converts a FZP angle into the PWS system.
+        /// </summary>
+        /// <param name="angle">FZP angle.</param>
+        /// <returns>Float with the equivalent PWS angle.</returns>
+        private float ConvertFZPAngle(float angle)
+        {
+            var converted = 180f - angle;
+            if (converted < 0)
+                converted += 360f;
+            return converted;
+        }
+
+        /// <summary>
+        /// Writes the metadata file for the PWS output. 
+        /// </summary>
+        /// <param name="times">Full list of times from the traffic simulation.</param>
+        /// <param name="outputFilenames">Full list of the output filenames and its first and last timestep.</param>
+        /// <param name="writer">Writer of the output file.</param>
+        private void WriteMetaData(List<float> times, List<string> outputFilenames, StreamWriter writer)
+        {
+            Console.WriteLine("Writing meta file");
+
+            string timesMeta = "TIMES ";
+
+            for (int i = 0; i < times.Count; i++)
+                timesMeta = timesMeta + times[i] + " ";
+
+            writer.WriteLine(timesMeta);
+
+            for (int i = 0; i < outputFilenames.Count; i++)
+            {
+                writer.WriteLine("FILENAME " + outputFilenames[i]);
+            }
+        }
+
+        /// <summary>
+        /// Writes a new timestep in an PWS output file. 
+        /// </summary>
+        /// <param name="currentTimeStep">Timestep to write.</param>
+        /// <param name="vehiclesOfCurrentTimeStep">List of vehicles in the timestep.</param>
+        /// <param name="writer">Writer of the output file.</param>
+        private void WriteNewTimeStep(int currentTimeStep, List<string> vehiclesOfCurrentTimeStep, StreamWriter writer)
+        {
+            string line = currentTimeStep + ";" + vehiclesOfCurrentTimeStep.Count + ";";
+
+            for (int i = 0; i < vehiclesOfCurrentTimeStep.Count; i++)
+            {
+                line = line + vehiclesOfCurrentTimeStep[i] + ";";
+            }
+
+            writer.WriteLine(line);
+        }
+    }
+}
