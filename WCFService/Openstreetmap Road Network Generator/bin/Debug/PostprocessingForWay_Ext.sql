789c5777dc093f0b890dbf897fd68ea9aa0dba4a
Nathan van Ofwegen <nj.vanofwegen@gmail.com>
2016-08-29 15:08:27 +0200

    Mass deletion of all build files

--- WCFService/Openstreetmap Road Network Generator/bin/Debug/PostprocessingForWay_Ext.sql
+++
@@ -1,5 +0,0 @@
-﻿			-- Things to run after populating way_ext table
-			-- way ext node info aram
-			delete from wayextnodeinfoaram_cachedfromview;
-			insert into wayextnodeinfoaram_cachedfromview
-			select * from viewwayextnodeinfoaram;
