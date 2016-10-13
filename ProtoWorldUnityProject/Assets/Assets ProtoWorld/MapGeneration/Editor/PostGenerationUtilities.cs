bf565a76bb8e6a28313d8a44f9046bc7e153de9d
Berend Wouda <b.j.wouda@tudelft.nl>
2016-10-13 19:06:16 +0200

    Made the script super nice with a progress bar and all... and optimized the Delft scene using the new tool, and by combining buildings and combining roads.
    But it's still 600 MB, I dunno if that makes it very useful. It's only 250 MB smaller than Rome.

--- ProtoWorldUnityProject/Assets/Assets ProtoWorld/MapGeneration/Editor/PostGenerationUtilities.cs
+++ ProtoWorldUnityProject/Assets/Assets ProtoWorld/MapGeneration/Editor/PostGenerationUtilities.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 using UnityEditor;
 
 public class Utilities : MonoBehaviour {
-	[MenuItem("ProtoWorld Editor/ProtoWorld Essentials/Map Tools/Utilities/Remove Game Objects with Vertices with Non-finite Values")]
-	private static void RemoveGameObjectsWithVerticesWithNonFiniteValues() {
-		Debug.Log("Removing game objects with vertices with non-finite values...");
+	[MenuItem("ProtoWorld Editor/ProtoWorld Essentials/Map Tools/Utilities/Remove Roads with Vertices with Non-finite Values")]
+	private static void RemoveRoadsWithVerticesWithNonFiniteValues() {
+		Debug.Log("Removing roads with vertices with non-finite values...");
 
-		GameObject[] gameObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+		GameObject[] roads = GameObject.FindGameObjectsWithTag("Line");
 
-		List<GameObject> gameObjectsWithVerticesWithInifiniteValues = new List<GameObject>();
+		List<GameObject> roadsWithVerticesWithNonFiniteValues = new List<GameObject>();
 
-		foreach(GameObject gameObject in gameObjects) {
+		for(int roadIndex = 0; roadIndex < roads.Length; roadIndex++) {
+			if(EditorUtility.DisplayCancelableProgressBar("Removing roads with vertices with non-finite values...", "", roadIndex / (float) roads.Length)) {
+				return;
+			}
+
+			GameObject gameObject = roads[roadIndex];
+
 			MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
-
+			
 			if(meshFilter != null && meshFilter.sharedMesh != null && meshFilter.sharedMesh.vertices != null) {
-				for(int index = 0; index < meshFilter.sharedMesh.vertexCount; index++) {
-					Vector3 vertex = meshFilter.sharedMesh.vertices[index];
+				for(int vertexIndex = 0; vertexIndex < meshFilter.sharedMesh.vertexCount; vertexIndex++) {
+					Vector3 vertex = meshFilter.sharedMesh.vertices[vertexIndex];
 					
 					if(float.IsInfinity(vertex.x) || float.IsNaN(vertex.x) || float.IsInfinity(vertex.y) || float.IsNaN(vertex.y) || float.IsInfinity(vertex.z) || float.IsNaN(vertex.z)) {
-						gameObjectsWithVerticesWithInifiniteValues.Add(gameObject);
+						roadsWithVerticesWithNonFiniteValues.Add(gameObject);
 
 						// Only add each game object once.
 						break;
@@ -30,17 +36,19 @@ public class Utilities : MonoBehaviour {
 			}
 		}
 
-		while(gameObjectsWithVerticesWithInifiniteValues.Count > 0) {
-			GameObject gameObject = gameObjectsWithVerticesWithInifiniteValues[0];
+		while(roadsWithVerticesWithNonFiniteValues.Count > 0) {
+			GameObject gameObject = roadsWithVerticesWithNonFiniteValues[0];
 
-			gameObjectsWithVerticesWithInifiniteValues.RemoveAt(0);
+			roadsWithVerticesWithNonFiniteValues.RemoveAt(0);
 
-			Debug.Log("Removing " + gameObject.name + "...");
+			Debug.Log("Removing road \"" + gameObject.name + "\"...");
 
 			DestroyImmediate(gameObject);
 			gameObject = null;
 		}
 
-		Debug.Log("Removed game objects with vertices with non-finite values.");
+		Debug.Log("Removed roads with vertices with non-finite values.");
+
+		EditorUtility.ClearProgressBar();
 	}
 }
