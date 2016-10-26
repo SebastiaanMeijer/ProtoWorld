1695fedb16b4f8bbb36617f6bfad6a63a1d73daa
Berend Wouda <b.j.wouda@tudelft.nl>
2016-10-26 18:46:19 +0200

    In order to make micro/macro work satisfyingly I added various forms of layer assignment. - Layers are assigned upon generation of buildings or lines (roads). They are assigned to the objects as well as the group. This way, these groups can be dragged into the micro/macro visualiser. The Lines layer is only assigned to the Lines group, since roads have their own layers already. As such the visualiser thing doesn't work for them, but currrently these are never hidden as part of the micro/macro thing. You can still hide them if neededthough, by using individual example roads themselves (one for each layer). - There are now tools in the Utilities menu to assign layers to previously generated Buildings and Lines if they don't have them yet. Roads should always have layers assigned already, so this doesn't mess with that. This also sets them on the groups if they don't have them yet. - Layers are assigned to grouped meshes based on their material (it compares to the materials of the originals). - There are now tools in the Utilities menu to assign layers to previously combined Buildings and Lines if they don't have them yet. - There are now tools in the Utilities menu to remove the "empty" buildings and roads that remain after all the optimization procedures. This doesn't improve performance much, but it reduces the file size of the scene.

--- ProtoWorldUnityProject/Assets/Assets ProtoWorld/MapGeneration/Editor/PostGenerationUtilities.cs
+++ ProtoWorldUnityProject/Assets/Assets ProtoWorld/MapGeneration/Editor/PostGenerationUtilities.cs
@@ -5,29 +5,38 @@ using UnityEngine;
 using UnityEditor;
 
 public class Utilities : MonoBehaviour {
+	[MenuItem("ProtoWorld Editor/ProtoWorld Essentials/Map Tools/Utilities/Remove Buildings with Vertices with Non-finite Values")]
+	public static void RemoveBuildingsWithVerticesWithNonFiniteValues() {
+		RemoveGameObjectsWithVerticesWithNonFiniteValues("building", "buildings", "Building");
+	}
+
 	[MenuItem("ProtoWorld Editor/ProtoWorld Essentials/Map Tools/Utilities/Remove Roads with Vertices with Non-finite Values")]
-	private static void RemoveRoadsWithVerticesWithNonFiniteValues() {
-		Debug.Log("Removing roads with vertices with non-finite values...");
+	public static void RemoveRoadsWithVerticesWithNonFiniteValues() {
+		RemoveGameObjectsWithVerticesWithNonFiniteValues("road", "roads", "Line");
+	}
 
-		GameObject[] roads = GameObject.FindGameObjectsWithTag("Line");
+	private static void RemoveGameObjectsWithVerticesWithNonFiniteValues(string singularName, string pluralName, string tag) {
+		Debug.Log("Removing " + pluralName + " with vertices with non-finite values...");
 
-		List<GameObject> roadsWithVerticesWithNonFiniteValues = new List<GameObject>();
+		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(tag);
 
-		for(int roadIndex = 0; roadIndex < roads.Length; roadIndex++) {
-			if(EditorUtility.DisplayCancelableProgressBar("Removing roads with vertices with non-finite values...", "", roadIndex / (float) roads.Length)) {
+		List<GameObject> gameObjectsWithVerticesWithNonFiniteValues = new List<GameObject>();
+
+		for(int gameObjectIndex = 0; gameObjectIndex < gameObjects.Length; gameObjectIndex++) {
+			if(EditorUtility.DisplayCancelableProgressBar("Removing " + pluralName + " with vertices with non-finite values...", "", gameObjectIndex / (float) gameObjects.Length)) {
 				return;
 			}
 
-			GameObject gameObject = roads[roadIndex];
+			GameObject gameObject = gameObjects[gameObjectIndex];
 
 			MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
-			
+
 			if(meshFilter != null && meshFilter.sharedMesh != null && meshFilter.sharedMesh.vertices != null) {
 				for(int vertexIndex = 0; vertexIndex < meshFilter.sharedMesh.vertexCount; vertexIndex++) {
 					Vector3 vertex = meshFilter.sharedMesh.vertices[vertexIndex];
-					
+
 					if(float.IsInfinity(vertex.x) || float.IsNaN(vertex.x) || float.IsInfinity(vertex.y) || float.IsNaN(vertex.y) || float.IsInfinity(vertex.z) || float.IsNaN(vertex.z)) {
-						roadsWithVerticesWithNonFiniteValues.Add(gameObject);
+						gameObjectsWithVerticesWithNonFiniteValues.Add(gameObject);
 
 						// Only add each game object once.
 						break;
@@ -36,19 +45,154 @@ public class Utilities : MonoBehaviour {
 			}
 		}
 
-		while(roadsWithVerticesWithNonFiniteValues.Count > 0) {
-			GameObject gameObject = roadsWithVerticesWithNonFiniteValues[0];
+		while(gameObjectsWithVerticesWithNonFiniteValues.Count > 0) {
+			GameObject gameObject = gameObjectsWithVerticesWithNonFiniteValues[0];
 
-			roadsWithVerticesWithNonFiniteValues.RemoveAt(0);
+			gameObjectsWithVerticesWithNonFiniteValues.RemoveAt(0);
 
-			Debug.Log("Removing road \"" + gameObject.name + "\"...");
+			Debug.Log("Removing " + singularName + " \"" + gameObject.name + "\"...");
 
 			DestroyImmediate(gameObject);
 			gameObject = null;
 		}
 
-		Debug.Log("Removed roads with vertices with non-finite values.");
+		EditorUtility.ClearProgressBar();
+
+		Debug.Log("Removed " + pluralName + " with vertices with non-finite values.");
+	}
+
+
+	[MenuItem("ProtoWorld Editor/ProtoWorld Essentials/Map Tools/Utilities/Assign Missing Layers to Buildings")]
+	public static void AssignMissingLayersToBuildings() {
+		AssignMissingLayersToGameObjects("building", "buildings", "Buildings", "Building", "Buildings");
+	}
+
+	[MenuItem("ProtoWorld Editor/ProtoWorld Essentials/Map Tools/Utilities/Assign Missing Layers to Roads")]
+	public static void AssignMissingLayersToRoads() {
+		AssignMissingLayersToGameObjects("road", "roads", "Lines", "Line", "Lines");
+	}
+
+	private static void AssignMissingLayersToGameObjects(string singularName, string pluralName, string name, string tag, string layer) {
+		Debug.Log("Assigning missing layers to " + pluralName + "...");
+
+		GameObject group = GameObject.Find(name);
+
+		if(group.layer == LayerMask.NameToLayer("Default")) {
+			group.layer = LayerMask.NameToLayer(layer);
+		}
+
+		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(tag);
+
+		for(int index = 0; index < gameObjects.Length; index++) {
+			if(EditorUtility.DisplayCancelableProgressBar("Assigning missing layers to " + pluralName + "...", "", index / (float) gameObjects.Length)) {
+				return;
+			}
+
+			GameObject gameObject = gameObjects[index];
+
+			if(gameObject.layer == LayerMask.NameToLayer("Default")) {
+				gameObject.layer = LayerMask.NameToLayer(layer);
+			}
+		}
 
 		EditorUtility.ClearProgressBar();
+
+		Debug.Log("Assigned missing layers to " + pluralName + ".");
+	}
+
+
+	[MenuItem("ProtoWorld Editor/ProtoWorld Essentials/Map Tools/Utilities/Assign Missing Layers to Grouped Buildings")]
+	public static void AssignMissingLayersToGroupedBuildings() {
+		AssignMissingLayersToGroupedGameObjects("building", "buildings", "CombinedBuildings");
+	}
+
+	[MenuItem("ProtoWorld Editor/ProtoWorld Essentials/Map Tools/Utilities/Assign Missing Layers to Grouped Roads")]
+	public static void AssignMissingLayersToGroupedRoads() {
+		AssignMissingLayersToGroupedGameObjects("road", "roads", "CombinedLines");
+	}
+
+	private static void AssignMissingLayersToGroupedGameObjects(string singularName, string pluralName, string tag) {
+		Debug.Log("Assigning missing layers to grouped " + pluralName + "...");
+
+		GameObject[] combinations = GameObject.FindGameObjectsWithTag(tag);
+
+		for(int combinationsIndex = 0; combinationsIndex < combinations.Length; combinationsIndex++) {
+			if(EditorUtility.DisplayCancelableProgressBar("Assigning missing layers to grouped " + pluralName + "...", "", combinationsIndex / (float) combinations.Length)) {
+				return;
+			}
+
+			GameObject combination = combinations[combinationsIndex];
+
+			// Set the layer to the layer the first matching child is in, which is likely
+			// to be the layer all the children are in, since their materials are the same.
+			if(combination.layer == LayerMask.NameToLayer("Default")) {
+				for(int childIndex = 0; childIndex < combination.transform.parent.childCount; childIndex++) {
+					GameObject child = combination.transform.parent.GetChild(childIndex).gameObject;
+
+					if(child.tag != tag) {
+						if(child.GetComponent<Renderer>().sharedMaterial.name == combination.GetComponent<Renderer>().sharedMaterial.name) {
+							Debug.Log("Assigning missing layer \"" + LayerMask.LayerToName(child.layer) + "\" to grouped " + singularName + " \"" + combination.name + "\"...");
+
+							combination.layer = child.layer;
+
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		EditorUtility.ClearProgressBar();
+
+		Debug.Log("Assigned missing layers to grouped " + pluralName + ".");
+	}
+
+
+	[MenuItem("ProtoWorld Editor/ProtoWorld Essentials/Map Tools/Utilities/Remove Empty Buildings")]
+	public static void RemoveEmptyBuildings() {
+		RemoveEmptyGameObjects("building", "buildings", "Building");
+	}
+
+	[MenuItem("ProtoWorld Editor/ProtoWorld Essentials/Map Tools/Utilities/Remove Empty Roads")]
+	public static void RemoveEmptyRoads() {
+		RemoveEmptyGameObjects("road", "roads", "Line");
+	}
+
+	private static void RemoveEmptyGameObjects(string singularName, string pluralName, string tag) {
+		Debug.Log("Removing empty " + pluralName + "...");
+
+		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(tag);
+
+		List<GameObject> emptyGameObjects = new List<GameObject>();
+
+		for(int gameObjectIndex = 0; gameObjectIndex < gameObjects.Length; gameObjectIndex++) {
+			if(EditorUtility.DisplayCancelableProgressBar("Removing empty " + pluralName + "...", "", gameObjectIndex / (float) gameObjects.Length)) {
+				return;
+			}
+
+			GameObject gameObject = gameObjects[gameObjectIndex];
+
+			MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
+			MeshCollider meshCollider = gameObject.GetComponent<MeshCollider>();
+
+			if(!meshRenderer.enabled && meshCollider == null) {
+				emptyGameObjects.Add(gameObject);
+			}
+		}
+
+		while(emptyGameObjects.Count > 0) {
+			GameObject gameObject = emptyGameObjects[0];
+
+			emptyGameObjects.RemoveAt(0);
+
+			Debug.Log("Removing " + singularName + " \"" + gameObject.name + "\"...");
+
+			DestroyImmediate(gameObject);
+			gameObject = null;
+		}
+
+		EditorUtility.ClearProgressBar();
+
+		Debug.Log("Removed empty " + pluralName + ".");
 	}
 }
