be954d992c6e26649aaa241e54ac4fc58d4c9779
Berend Wouda <b.j.wouda@tudelft.nl>
2016-10-26 21:45:08 +0200

    Addressed small unreadability in KPI delays code.

--- ProtoWorldUnityProject/Assets/Assets ProtoWorld/Transportations/Scripts/KPIDelays.cs
+++ ProtoWorldUnityProject/Assets/Assets ProtoWorld/Transportations/Scripts/KPIDelays.cs
@@ -29,8 +29,10 @@ public class KPIDelays : MonoBehaviour
         float tmpBusDelay = 0;
         float tmpMetroDelay = 0;
 
-        foreach (Transform transLine in transLines){
-        foreach(Transform item in transLine){
+        foreach (Transform transLine in transLines)
+		{
+            foreach (Transform item in transLine)
+			{
                 VehicleController vc = item.GetComponent<VehicleController>();
                 if (transLine.name.StartsWith("Tram_"))
                     tmpTramDelay += vc.delay;
