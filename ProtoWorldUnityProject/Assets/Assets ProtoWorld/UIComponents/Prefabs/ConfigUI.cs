797ab0ca077bf438357a6013948481b62e9cf221
Nathan van Ofwegen <nj.vanofwegen@gmail.com>
2016-10-31 17:46:09 +0100

    Config button for activating/deactivating UICanvas objects

---
+++ ProtoWorldUnityProject/Assets/Assets ProtoWorld/UIComponents/Prefabs/ConfigUI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ConfigUI : MonoBehaviour
+{
+
+    private bool active = false;
+    public List<GameObject> gameObjects;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	        
+	}
+
+
+    public void Toggle()
+    {
+        active = !active;
+        foreach (GameObject o in gameObjects)
+        {
+            if(o != null) o.SetActive(active);
+        }
+    }
+
+}
