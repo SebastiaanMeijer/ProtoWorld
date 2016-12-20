2b9098a6ee27689cc407a6e6e50974bbeb2e75e9
Berend Wouda <b.j.wouda@tudelft.nl>
2016-12-20 22:12:00 +0100

    Moved the ConfigUI functionality to a separate module called the OverlayModule. It works largely the same, but starts out with the configured setting and has a huge-ass button (as requested) at the center-bottom of the screen with a configurable text.

---
+++ ProtoWorldUnityProject/Assets/Assets ProtoWorld/OverlayModule/Scripts/Overlay.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Overlay : MonoBehaviour {
+	public GameObject[] gameObjects;
+
+	public bool active;
+
+	public string enableTitle;
+	public string disableTitle;
+
+	public Text text;
+
+
+	public void Start() {
+		updateButton();
+		updateOverlay();
+	}
+
+
+	public void Toggle() {
+		active = !active;
+
+		updateButton();
+		updateOverlay();
+	}
+
+
+	private void updateButton() {
+		if(active) {
+			text.text = disableTitle;
+		}
+		else {
+			text.text = enableTitle;
+		}
+	}
+	
+	private void updateOverlay() {
+		foreach(GameObject gameObject in gameObjects) {
+			if(gameObject != null) {
+				gameObject.SetActive(active);
+			}
+		}
+	}
+}
