09d9109f0a035e9061b8502b32f2ad2ad547133e
Berend Wouda <b.j.wouda@tudelft.nl>
2016-12-23 22:03:39 +0100

    Added a menu, and transitions between the scenes to hide the stutter. Changed and added the general purpose buttons in the scenes (reset camera, now also return to menu and exit). Disabled the configuration dialog that would pop up when playing a build. Fixed various issues I ran into (I committed them separately as much as possible), especially when building scenes.
    Changed some icon texture settings to "Clamp" to prevent edge bleeding.
    
    Fixed some small things in the Rome Case that I saved up to save on committing tons of gigabytes. I don't even remember what they are now :P Maybe I even committed them!

---
+++ ProtoWorldUnityProject/Assets/Assets ProtoWorld/UIComponents/Scripts/ExitToDesktop.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ExitToDesktop : MonoBehaviour {
+	public TransitionController transitionController;
+
+
+	public void exitToDesktop() {
+		transitionController.transitionOut(exit);
+	}
+
+	private void exit() {
+		// TODO This is a hack to prevent Unity from crashing upon exit. It is likely caused by one
+		// of the modules as it only happens in the case scenes, but no errors are being logged.
+		// For now, this will have to do, as we are closing the application anwyays.
+		if(!Application.isEditor) {
+			System.Diagnostics.Process.GetCurrentProcess().Kill();
+		}
+
+		//Application.Quit();
+	}
+}
---
+++ ProtoWorldUnityProject/Assets/Assets ProtoWorld/UIComponents/Scripts/TransitionController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransitionController : MonoBehaviour {
+	public bool useTransition;
+
+	private Image image;
+
+	private WaitForSeconds transitionWait;
+	private WaitForSeconds transitionInterval;
+
+
+	public void Awake() {
+		image = GetComponent<Image>();
+
+		if(useTransition) {
+			image.enabled = true;
+		}
+	}
+
+	void Start () {
+		transitionWait = new WaitForSeconds(1.0f);
+		transitionInterval = new WaitForSeconds(0.01f);
+		
+		transitionIn();
+	}
+
+
+	public void transitionIn(Action action = null) {
+		if(useTransition) {
+			StartCoroutine(startInTransition(action));
+		}
+	}
+
+	public void transitionOut(Action action = null) {
+		if(useTransition) {
+			StartCoroutine(startOutTransition(action));
+		}
+	}
+
+
+	private IEnumerator startInTransition(Action action) {
+		yield return transitionWait;
+		
+		for(int time = 0; time <= 25; time++) {
+			setInTransitionTime(time / 25.0f);
+
+			yield return transitionInterval;
+		}
+
+		// No longer block raycasts.
+		image.enabled = false;
+
+		if(action != null) {
+			action();
+		}
+	}
+	
+	private IEnumerator startOutTransition(Action action) {
+		// Block raycasts.
+		image.enabled = true;
+		
+		for(int time = 0; time <= 25; time++) {
+			setOutTransitionTime(time / 25.0f);
+
+			yield return transitionInterval;
+		}
+
+		yield return transitionWait;
+		
+		if(action != null) {
+			action();
+		}
+	}
+
+
+	private void setInTransitionTime(float time) {
+		Color color = image.color;
+		color.a = 1.0f - time;
+		image.color = color;
+	}
+	
+	private void setOutTransitionTime(float time) {
+		Color color = image.color;
+		color.a = time;
+		image.color = color;
+	}
+}
