32da44cec18a7550ff88dcb160d522517c4d4d45
Berend Wouda <b.j.wouda@tudelft.nl>
2016-12-29 13:32:52 +0100

    Added a sprite controller and a text controller script. They can be used to billboard sprites or text in the scene. They aways appear to be the same size and rotation to the camera, and they are hidden when the camera gets too close.

---
+++ ProtoWorldUnityProject/Assets/Assets ProtoWorld/UIComponents/Scripts/SpriteController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class SpriteController : MonoBehaviour {
+	public float scale = 1.0f;
+	public float hideDistance = 10.0f;
+
+
+	public void Update () {
+		transform.rotation = Camera.main.transform.rotation;
+		transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+		SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+		Sprite sprite = spriteRenderer.sprite;
+
+		Vector3 center = sprite.bounds.center;
+		Vector3 minimum = sprite.bounds.min;
+
+		Vector3 centerPosition = transform.TransformPoint(center);
+		Vector3 minimumPosition = transform.TransformPoint(minimum);
+
+		Vector3 screenCenterPosition = Camera.main.WorldToScreenPoint(centerPosition);
+		Vector3 screenMinimumPosition = Camera.main.WorldToScreenPoint(minimumPosition);
+
+		screenCenterPosition.z = 0.0f;
+		screenMinimumPosition.z = 0.0f;
+
+		float pixelDistance = Mathf.Sqrt(sprite.texture.height * sprite.texture.height / 4.0f + sprite.texture.width * sprite.texture.width / 4.0f);
+		float screenDistance = Vector3.Distance(screenCenterPosition, screenMinimumPosition);
+
+		float localScale = pixelDistance * scale / screenDistance;
+
+		transform.localScale = new Vector3(localScale, localScale, localScale);
+
+		spriteRenderer.enabled = Vector3.Distance(Camera.main.transform.position, transform.position) > hideDistance;
+	}
+}
---
+++ ProtoWorldUnityProject/Assets/Assets ProtoWorld/UIComponents/Scripts/TextController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class TextController : MonoBehaviour {
+	public float scale = 1.0f;
+	public float hideDistance = 10.0f;
+
+	public float height = 0.1f;
+
+
+	public void Update () {
+		transform.rotation = Camera.main.transform.rotation;
+		transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		
+		MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+		
+		Vector3 center = new Vector3(0.0f, 0.0f, 0.0f);
+		Vector3 minimum = new Vector3(-height / 2.0f, -height / 2.0f, 0.0f);
+
+		Vector3 centerPosition = transform.TransformPoint(center);
+		Vector3 minimumPosition = transform.TransformPoint(minimum);
+
+		Vector3 screenCenterPosition = Camera.main.WorldToScreenPoint(centerPosition);
+		Vector3 screenMinimumPosition = Camera.main.WorldToScreenPoint(minimumPosition);
+
+		screenCenterPosition.z = 0.0f;
+		screenMinimumPosition.z = 0.0f;
+		
+		float screenDistance = Vector3.Distance(screenCenterPosition, screenMinimumPosition);
+
+		float localScale = scale / screenDistance;
+
+		transform.localScale = new Vector3(localScale, localScale, localScale);
+
+		meshRenderer.enabled = Vector3.Distance(Camera.main.transform.position, transform.position) > hideDistance;
+	}
+}
